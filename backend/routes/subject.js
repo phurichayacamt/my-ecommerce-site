61efc1627038545f8225dfbecbaeb81aa6736432
phurichayacamt <kingphurichayaaaaa@gmail.com>
2025-05-14 19:14:29 +0700

    เตรียมโค้ดพัง

--- backend/routes/subject.js
+++ backend/routes/subject.js
@@ -1,10 +1,26 @@
+// const express = require('express');
+// const router = express.Router();
+// const subject = require('../data/contact_subject.json');
+
+// router.get('/', (req, res) => {
+//     res.end('{"contactSubject": ["General Enquiry", "Class", "Schedule","Instructor","Price","Other"] }');
+//     req.json(subject);
+// });
+
+// module.exports = router;
+
 const express = require('express');
-const router = express.Router();
+const Router = express.Router();
+
+//const subject = '{"contactSubject": ["General Enquery","Classes","Schedules","Instructor","Prices","Other","Nattharin","Pin"]}';
 const subject = require('../data/contact_subject.json');
 
-router.get('/', (req, res) => {
-    res.end('{"contactSubject": ["General Enquiry", "Class", "Schedule","Instructor","Price","Other"] }');
-    req.json(subject);
+Router.get('/', (req, res) => {
+  //res.end(subject); // string (text)
+  res.json(subject); // file
 });
 
-module.exports = router;
+module.exports = Router;
+﻿
+
+ 
--- backend/routes/subscribe.js
+++ backend/routes/subscribe.js
@@ -4,34 +4,30 @@ const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 
-
-router.post('/',(req,res) =>{
-    const{email} = req.body
-    const subscribe = {subscribeAt: new Date(),email};
+router.post('/',(req,res) => {
+    const {email} = req.body;
+    const subscribe = {subscribeAt : new Date(), email};
 
     const filePath = path.join(__dirname,'..','data','subscribe.json');
-     console.log('Content form submited',{email});
 
-     let subscribes = [];
-    
-
-     if(fs.existsSync(filePath)){
-        //have 
-        const filedata = fs.readFileSync(filePath,'utf-8');
-        subscribes = JSON.parse(filePath);
+    let subscribes = [];
+    if(fs.existsSync(filePath)){
+         //file is there
+        const filedata = fs.readFileSync(filePath, 'utf-8');
+        subscribes =JSON.parse(filedata);
+        subscribes.push(subscribe);
+        fs.writeFileSync(filePath, JSON.stringify(subscribes, null, 2));
+        res.status(200).json({status : "Message Received"});
+        console.log('Content from submited', {email});
+    }else{
+        //no file
         subscribes.push(subscribe)
-        fs.writeFileSync(filePath,JSON.stringify(subscribes,null,2));
-        res.status(200).json ({status: "Message Recived)"});
-        console.log('Content form submited',{email});
-     }else{
-        //no 
-        subscribes.push(subscribe)
-        fs.writeFileSync(filePath,JSON.stringify(subscribes,null,2));
-        res.status(200).json ({status: "Message Recived)"});
-        console.log('Content form submited',{email});
+        fs.writeFileSync(filePath, JSON.stringify(subscribes, null, 2));
+        res.status(200).json({status : "Message Received"});
+        console.log('Content from submited', {email});
+    }
 
-     }
 
-})
+});
 
 module.exports = router;
